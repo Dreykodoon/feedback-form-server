838b7f1e42112f44a68d9d7cadcde443de5620b7
Mihai Cioban <mcioban@axway.com>
2017-09-22 14:54:02 +0300

    Request body now parsed correctly.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 const router = express.Router();
 
 router.post('/', function(req, res) {
+    console.log(req.body);
     res.send('received test');
 });
 

26af735fba414b27126ca7411b39145a46c5e46a
Mihai Cioban <mcioban@axway.com>
2017-10-23 08:52:07 +0300

    Simplified error message when form filled incorrectly.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('./logger');
-const {isFormValid, generateErrorMessage} = require('./form-validation');
+const {isFormValid} = require('./form-validation');
 
 const app = express();
 
@@ -11,14 +11,14 @@ const router = express.Router();
 
 router.post('/', function(req, res) {
     if (isFormValid(req.body)) {
-        // Security measure for stopping form spamming.
+        // Log any spamming attempt.
         if (req.body.email2) {
             logger.log('warn', req.body);
         }
         res.send('Message forwarded successfully!');
     }
     else {
-        res.status(400).send({error: generateErrorMessage(req.body)});
+        res.status(400).send({error: 'Error while sending message!'});
     }
 });
 

19f45bbb2253d31ac9c268e7184e35438d92ac08
Mihai <ciobanmihai@gmail.com>
2017-11-12 13:44:05 +0200

    Email won't be sent if secure connection cannot be established

--- src/configurations/nodemailer-config.js
+++ src/configurations/nodemailer-config.js
@@ -4,6 +4,9 @@ const transporter = nodemailer.createTransport({
     host:   'smtp.gmail.com',
     port:   587,
     secure: false, // true for 465, false for other ports
+    /* if 'requireTLS' is true and 'secure' is false then Nodemailer tries to use STARTTLS even
+        if the server does not advertise support for it. If the connection can not be encrypted then message is not sent */
+    requireTLS: true,
     auth:   {
         user: process.env.EMAIL_SENDER,
         pass: process.env.EMAIL_PASS,

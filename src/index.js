7e361ecf0bed014f1745ca34b9dcc84cddffeb05
Mihai <ciobanmihai@gmail.com>
2017-11-17 13:10:08 +0200

    Better complience with RESTful resource naming practices

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const logger = require('./configurations/logger-config');
-const emailRouter = require('./routes/email-router');
+const resourcesRouter = require('./routes/resources-router');
 const {checkEnvironmentVariables} = require('./env-validation');
 
 checkEnvironmentVariables();
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(session({saveUninitialized: false, resave: false, secret: 'NOT_SO_SECRET', cookie: {maxAge: 600000}}));
 
-app.use('/app', emailRouter);
+app.use('/resources', resourcesRouter);
 
 app.listen(8080, function() {
     logger.log('info', 'Feedback Form server started. Listening on port 8080!');
---
+++ src/routes/resources-router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const emailRouter = require('./email-router');
+
+const router = express.Router();
+
+router.use('/email', emailRouter);
+
+module.exports = router;
